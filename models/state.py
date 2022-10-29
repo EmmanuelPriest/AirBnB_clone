03e1056d4bb8a259e7c6116c5dbcd165e2570f55
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-10-29 06:08:14 -0700

    class State that inherits from class BaseModel

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''Defines the class State'''
+from model.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''class State that inheritted class BaseModel
+
+    Attribute:
+        name (str): The name of the state
+
+    '''
+
+    name = ""
