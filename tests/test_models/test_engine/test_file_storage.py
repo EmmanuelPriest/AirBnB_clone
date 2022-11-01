e0be713e802a2340e0cffea94f0efb3e2539266f
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2022-10-31 17:21:58 -0700

    correction

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,7 +10,13 @@ from models import storage
 import re
 import json
 import os
-
+import pep8
+from models.user import User
+from models.state import State
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class TestFileStorage(unittest.TestCase):
     '''Test Cases for the FileStorage class'''
